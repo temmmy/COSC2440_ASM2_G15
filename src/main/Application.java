a64f96e27b8d6f193a24fecc4d56190a32fb3dd5
mushz <s3981278@rmit.edu.vn>
2024-04-30 22:24:31 +0700

    Remove Test Function

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,6 +1,5 @@
 package main;
 
-import main.dataStructure.Point;
 import main.model.Map2D;
 import main.model.Place;
 import main.model.ServiceType;
@@ -11,8 +10,9 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         System.out.println("1. Start application");
-        System.out.println("2. Start testing");
-        System.out.println(">> Enter your choice");
+        System.out.println("2. Start load testing");
+        System.out.println("3. Start remove testing");
+        System.out.print(">> Enter your choice: ");
         try (Scanner sc = new Scanner(System.in)) {
             int choice = sc.nextInt();
             switch (choice) {
@@ -21,6 +21,10 @@ public class Application {
                     break;
                     case 2:
                         loadTesting();
+                        break;
+                case 3:
+                    removeTesting();
+                    break;
                 default:
                     System.out.println("Invalid choice");
             }
@@ -28,14 +32,13 @@ public class Application {
     }
 
     private static void loadTesting() {
-        long startTime = System.currentTimeMillis();
-
         Map2D map = new Map2D();
         System.out.print("Number of places: ");
         Scanner sc = new Scanner(System.in);
         int numPlaces = sc.nextInt();
         int failureCount = 0;
         System.out.println("Loading testing data...");
+        long startTime = System.currentTimeMillis();
         for (int i = 0; i < numPlaces; i++) {
             int x = i;
             int y = i;
@@ -55,6 +58,49 @@ public class Application {
         long elapsedTime = endTime - startTime;
         System.out.println("*".repeat(60));
         System.out.println("Loading testing data with " + numPlaces + " places finished in " + elapsedTime + " ms");
+        double failureRate = (double) failureCount / (double) numPlaces;
+        System.out.println("Failures: " + failureCount + ", Failure Rate: " + failureRate*100 + "%");
+        System.out.println("*".repeat(60));
+    }
+
+    private static void removeTesting() {
+
+        Map2D map = new Map2D();
+        System.out.print("Number of places: ");
+        Scanner sc = new Scanner(System.in);
+        int numPlaces = sc.nextInt();
+        int failureCount = 0;
+        System.out.println("Loading testing data...");
+        for (int i = 0; i < numPlaces; i++) {
+            int x = i;
+            int y = i;
+            String name = "Place " + i;
+            ServiceType[] services = ServiceType.values();
+            Place place = new Place(x, y, name, services);
+            map.insert(place);
+        }
+        sc.close();
+
+        long startTime = System.currentTimeMillis();
+
+        for (int i = 0; i < numPlaces; i++) {
+            int x = i;
+            int y = i;
+            String name = "Place " + i;
+            ServiceType[] services = ServiceType.values();
+            Place place = new Place(x, y, name, services);
+            if (map.remove(place)) {
+                System.out.println("Removed " + place);
+            } else {
+                failureCount++;
+                System.out.println(i + ". Removal failed");
+            }
+        }
+
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+        System.out.println("*".repeat(60));
+        System.out.println("Removing testing data with " + numPlaces + " places finished in " + elapsedTime + " ms");
         double failureRate = (double) failureCount / (double) numPlaces;
         System.out.println("Failures: " + failureCount + ", Failure Rate: " + failureRate*100 + "%");
         System.out.println("*".repeat(60));
